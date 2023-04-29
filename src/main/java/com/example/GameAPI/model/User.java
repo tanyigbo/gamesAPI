3af1505ee31581ff62f9401df3e123afa24a24fa
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-29 07:29:20 -0500

    made user model a jpa entitiy

--- src/main/java/com/example/GameAPI/model/User.java
+++ src/main/java/com/example/GameAPI/model/User.java
@@ -1,4 +1,77 @@
 package com.example.GameAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user",orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Game> gameList;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, List<Game> gameList) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.gameList = gameList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Game> getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(List<Game> gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", gameList=" + gameList +
+                '}';
+    }
 }
