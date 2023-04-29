0382bff2ce20389a553388c6390cb0f87c9da749
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-29 08:07:17 -0500

    made genre model a JPA entity connected to game

--- src/main/java/com/example/GameAPI/model/Genre.java
+++ src/main/java/com/example/GameAPI/model/Genre.java
@@ -1,2 +1,75 @@
-package com.example.GameAPI.model;public class Genre {
+package com.example.GameAPI.model;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column
+    private String description;
+
+    @OneToMany(mappedBy = "genre")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Game> gameList;
+
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name, String description, List<Game> gameList) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.gameList = gameList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Game> getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(List<Game> gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", gameList=" + gameList +
+                '}';
+    }
 }
