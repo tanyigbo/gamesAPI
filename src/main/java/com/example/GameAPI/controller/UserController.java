2d87fd2cf6150b0dd4b43111665c1624158e6c64
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-30 01:56:44 -0500

    refactored security configuration

--- src/main/java/com/example/GameAPI/controller/UserController.java
+++ src/main/java/com/example/GameAPI/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/users")
+@RequestMapping(path = "/auth/users")
 public class UserController {
 
     private final UserService userService;
