8977c55c033910c4540d36a62c462adc89804416
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-29 23:25:32 -0500

    added findActiveUser method

--- src/main/java/com/example/GameAPI/service/UserService.java
+++ src/main/java/com/example/GameAPI/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.GameAPI.service;
 
 import com.example.GameAPI.exception.InformationExistException;
+import com.example.GameAPI.exception.InformationNotFoundException;
 import com.example.GameAPI.model.User;
 import com.example.GameAPI.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ public class UserService {
      * Creates a new user object with the data from the provided userObject
      * If the user does not already exist, saves a new user object to the userRepository
      * Throws an exception if a user with provided username already exists
+     *
      * @param userObject A userObject containing the values for a new user
      * @return The newly created user
      */
@@ -33,7 +35,22 @@ public class UserService {
             newUser.setPassword(userObject.getPassword());
             return userRepository.save(newUser);
         } else {
-            throw new InformationExistException("Username " + userObject.getUsername() + " already in use." );
+            throw new InformationExistException("Username " + userObject.getUsername() + " already in use.");
+        }
+    }
+
+    /**
+     * Finds the user object with a username matching the provided String
+     * Throws an exception if a user with the provided username does not exist
+     * @param username Value of expected username
+     * @return A User with a matching username
+     */
+    public User findActiveUserByUsername(String username) {
+        Optional<User> user = userRepository.findUserByUsername(username);
+        if (user.isPresent()) {
+            return user.get();
+        }else {
+            throw new InformationNotFoundException("User with username  " + username + " was not found.");
         }
     }
 }
