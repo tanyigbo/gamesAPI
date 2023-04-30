babd618d2414b22299931bca811fc82764d858dc
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-29 20:28:45 -0500

    added userRepository findUserByUsername method

--- src/main/java/com/example/GameAPI/repository/UserRepository.java
+++ src/main/java/com/example/GameAPI/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.example.GameAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
+    // SELECT * FROM users WHERE username = name
+    Optional<User> findUserByUsername(String name);
 }
