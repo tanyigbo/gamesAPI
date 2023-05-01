67db29bc67108e343428113055484c3e6484d10a
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-01 02:20:15 -0500

    added findGameByName JPA method

--- src/main/java/com/example/GameAPI/repository/GameRepository.java
+++ src/main/java/com/example/GameAPI/repository/GameRepository.java
@@ -4,6 +4,11 @@ import com.example.GameAPI.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game,Long> {
+
+    // SELECT * FROM games WHERE name = gameName;
+    Optional<Game> findGameByName(String gameName);
 }
