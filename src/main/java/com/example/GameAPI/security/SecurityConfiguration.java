3a5e0087f1f1839b7233513bd80ca466fa5030d8
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-01 05:59:13 -0500

    added getGenres requet to filter

--- src/main/java/com/example/GameAPI/security/SecurityConfiguration.java
+++ src/main/java/com/example/GameAPI/security/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                        "/auth/users/register", "/auth/users/login")
+                        "/auth/users/register", "/auth/users/login", "/api/genres")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
