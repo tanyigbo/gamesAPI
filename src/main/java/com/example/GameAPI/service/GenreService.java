162b0d2e4e380f49822419747034cbbc7c9efcdf
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-30 22:46:26 -0500

    implemented findGenreById in GenreService class

--- src/main/java/com/example/GameAPI/service/GenreService.java
+++ src/main/java/com/example/GameAPI/service/GenreService.java
@@ -1,8 +1,12 @@
 package com.example.GameAPI.service;
 
+import com.example.GameAPI.exception.InformationNotFoundException;
+import com.example.GameAPI.model.Genre;
 import com.example.GameAPI.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GenreService {
@@ -12,5 +16,20 @@ public class GenreService {
     @Autowired
     public GenreService(GenreRepository genreRepository){
         this.genreRepository = genreRepository;
+    }
+
+    /**
+     * Finds the genre object with an id matching the provided genreID
+     * Throws and exception if no there is no genre found with a matching id
+     * @param genreId Id a genre object should have
+     * @return A genre object with id matching genreId
+     */
+    public Genre findGenreById(Long genreId){
+        Optional<Genre> genre = genreRepository.findGenreById(genreId);
+        if(genre.isPresent()){
+            return genre.get();
+        }else{
+            throw new InformationNotFoundException("Genre with ID " + genreId + " was not found.");
+        }
     }
 }
