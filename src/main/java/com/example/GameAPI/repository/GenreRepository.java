40565bea1ecf6cd64ce3d66d7151e6bbf51bcdc3
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-04-30 22:45:48 -0500

    added findGenreById JPA method

--- src/main/java/com/example/GameAPI/repository/GenreRepository.java
+++ src/main/java/com/example/GameAPI/repository/GenreRepository.java
@@ -4,6 +4,11 @@ import com.example.GameAPI.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
+
+    //SELECT * FROM genre WHERE id = genreId
+    Optional<Genre> findGenreById(Long genreId);
 }
